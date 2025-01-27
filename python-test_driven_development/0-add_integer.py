1811617ce051b2672bb15702dec91321853243f9
Falbert19 <fernandoalbert19@gmail.com>
2025-01-26 22:19:44 -0400

    module doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-   """This module contains a function to add two integers
+    """This module contains a function to add two integers
 
-The function takes two arguments, `a` and `b` (with `b` defaulting to 98).
-It casts both arguments to integers if they are floats, and raises a `TypeError`
-if either of the arguments is not an integer or a float
-"""
+    The function takes two arguments, `a` and `b` (with `b` defaulting to 98).
+    It casts both arguments to integers if they are floats,
+    and raises a `TypeError`
+    if either of the arguments is not an integer or a float
+    """
 
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
