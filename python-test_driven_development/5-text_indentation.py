3886dd53c833208ba15e7cebc24d0c74fb2b4edb
Falbert19 <fernandoalbert19@gmail.com>
2025-01-26 23:44:50 -0400

    task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ".", "?", and ":".
+    
+    Args:
+        text (str): The text to be processed.
+        
+    Raises:
+        TypeError: If the input text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    i = 0
+    while i < len(text):
+        if text[i] in '.?:':
+            print(text[i], end='')
+            print()
+            print()
+            i += 1
+        elif text[i] == ' ' and (i == 0 or text[i-1] in '.?:'):
+            i += 1
+        else:
+            print(text[i], end='')
+            i += 1
