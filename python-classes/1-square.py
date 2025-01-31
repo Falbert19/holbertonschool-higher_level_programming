26cc96609b46cb77a36e919333779fe694d67de2
Falbert19 <fernandoalbert19@gmail.com>
2025-01-31 18:01:01 -0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+this module defines a Square class
+"""
+
+
+class Square:
+    """
+    represents a Square
+
+    Attributes:
+    _size (init): size of square, private
+    """
+
+
+def __init__(self, size):
+    """
+    Init a Square instance
+
+    args:
+    size (int): size of square
+    """
+
+
+   #!/usr/bin/python3
+"""
+this module defines a Square class
+"""
+
+
+class Square:
+    """
+    represents a Square
+
+    Attributes:
+    _size (init): size of square, private
+    """
+
+
+    def __init__(self, size):
+        """
+        Init a Square instance
+
+        args:
+        size (int): size of square
+        """
+
+
+        self.__size = size
+
