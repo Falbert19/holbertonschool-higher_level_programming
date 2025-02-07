13e3b87b702768665309124180de2d1720911501
Falbert19 <fernandoalbert19@gmail.com>
2025-02-07 08:43:47 -0400

    task4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-this mdoule returns True if the object is an
+this returns True if the object is an
 instance of a class that inherited (directly or indirectly)
 from the specified class ; otherwise False
 """
