6fe57951ae985a9ce8716c7f72764242d67de196
Falbert19 <fernandoalbert19@gmail.com>
2025-02-07 08:42:07 -0400

    task4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this mdoule returns True if the object is an
+instance of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that
+    inherited from the specified class; otherwise, returns False
+
+    Args:
+        obj: object to check.
+        a_class: class to compare
+
+    Returns:
+        bool: True if obj is an instance of
+        a subclass of a_class, but not a_class itself
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
