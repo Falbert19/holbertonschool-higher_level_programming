2f08ab57757bb630d30b76412dcd28b0960fcb77
Falbert19 <fernandoalbert19@gmail.com>
2025-02-07 09:53:14 -0400

    task6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,6 +6,7 @@ Exception with the message area() is not implemented
 
 
 class BaseGeometry:
+    """class that raise a exception"""
     def area(self):
         """ Raises Exception message area() is not implemented """
         raise Exception("area() is not implemented")
