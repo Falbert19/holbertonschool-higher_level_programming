08dfdce24736eb77174ced32a6f6c029c2aec48b
Falbert19 <fernandoalbert19@gmail.com>
2025-02-09 23:00:17 -0400

    forms deltten

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,14 +2,14 @@
 """
 this returns True if the object is an
 instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False
+the specified class ; otherwise False
 """
 
 
 def inherits_from(obj, a_class):
     """
     returns True if the object is an instance of a class that
-    inherited from the specified class; otherwise, returns False
+    inherited  the specified class; otherwise, returns False
 
     Args:
         obj: object to check
