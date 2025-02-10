1a4cc0c83c18d8851d0eed9a875dff9fd4561e20
Falbert19 <fernandoalbert19@gmail.com>
2025-02-09 21:51:55 -0400

    redo rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,9 +4,15 @@ Module defining a Rectangle class that inherits from BaseGeometry.
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-class Rectangle:
+class Rectangle(BaseGeometry):
     """define a rectangle """
     def __init__(self, width, height):
+        """Initialize width and height with validation.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
