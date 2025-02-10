97fdaeb67edf47daec678dd86cbc4875dc39a843
Falbert19 <fernandoalbert19@gmail.com>
2025-02-09 23:22:47 -0400

    task redo

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -49,7 +49,7 @@ class Circle(Shape):
             raise TypeError("Radius must be a number")
         if radius < 0:
             raise ValueError("Radius cannot be negative")
-        self.radius = radius
+        self.radius = float(radius)
 
     def area(self) -> float:
         """
