a113865127edf3a03bf697cae157dea204c1aeff
Falbert19 <fernandoalbert19@gmail.com>
2025-02-13 01:54:37 -0400

    module doc

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+    Writes a string to a text file (UTF8)
+    and returns the number of characters written.
+
+    :param filename: Name of the file to write to.
+    :param text: The text to write into the file.
+    :return: Number of characters written.
+"""
+
+
 def write_file(filename="", text=""):
     with open(filename, mode="w", encoding="utf-8") as file:
-            return file.write(text)
+        return file.write(text)
