d68d18b575dcb91012782e4a286a37a69e545fb2
Falbert19 <fernandoalbert19@gmail.com>
2025-02-13 01:55:41 -0400

    module doc

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """
-    Writes a string to a text file (UTF8)
-    and returns the number of characters written.
-
-    :param filename: Name of the file to write to.
-    :param text: The text to write into the file.
-    :return: Number of characters written.
+Module for writing text to a file.
 """
 
 
 def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written.
+
+    This function ensures that:
+    - The file is created if it does not exist.
+    - The content of the file is overwritten if it already exists.
+    - The number of characters written is returned.
+
+    :param filename: Name of the file to write to.
+    :param text: The text to write into the file.
+    :return: Number of characters written.
+    """
     with open(filename, mode="w", encoding="utf-8") as file:
         return file.write(text)
