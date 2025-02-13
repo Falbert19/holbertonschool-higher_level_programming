13d88c2d3afb0b0c934344967ea682ebaa8ebc24
Falbert19 <fernandoalbert19@gmail.com>
2025-02-13 01:42:21 -0400

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,10 +2,12 @@
 """
 Module for reading and printing a text file.
 """
+
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
-    
+
     :param filename: Name of the file to read.
     """
     with open(filename, encoding="utf-8") as file:
