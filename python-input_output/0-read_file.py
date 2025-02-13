039b373e3824bdc1f94e1ded93cf4ae4b3f59016
Falbert19 <fernandoalbert19@gmail.com>
2025-02-13 01:41:23 -0400

    task0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for reading and printing a text file.
+"""
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    
+    :param filename: Name of the file to read.
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+This repo will contain task for inputs and outputs in python
