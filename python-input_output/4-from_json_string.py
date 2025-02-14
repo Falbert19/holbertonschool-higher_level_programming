c34e45295653451b60ee2921071a7cf674a644e2
Falbert19 <fernandoalbert19@gmail.com>
2025-02-14 19:01:06 -0400

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: from_json_string
+
+This module contains a function that returns a Python object from a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    :param my_str: The JSON string to deserialize.
+    :return: Corresponding Python object.
+    """
+    return json.loads(my_str)
