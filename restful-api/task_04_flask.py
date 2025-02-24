82fcadbef3be6c3afbae4a6610adf26a76a296d6
Falbert19 <fernandoalbert19@gmail.com>
2025-02-23 23:03:01 -0400

    ;2Ctask 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,56 +1,47 @@
 #!/usr/bin/python3
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
-
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+users = {}
 
 
 @app.route('/')
 def home():
-
     return "Welcome to the Flask API!"
 
 
 @app.route('/data')
 def get_data():
-
     return jsonify(list(users.keys()))
 
 
 @app.route('/status')
 def status():
-
     return "OK"
 
 
 @app.route('/users/<username>')
 def get_user(username):
-
-    if username in users:
-        return jsonify(users[username])
+    user = user.get(username):
+    if user:
+        return jsonify(user)
     else:
-        response = jsonify({"error": "User not found"})
-        response.status_code = 404
-        return response
+        return (jsonify({"error": "User not found"}), 404)
 
 
 @app.route('/add_user', methods=["POST"])
 def add_user():
-    data = request.get_json()
-    if not data or "username" not in data:
-        return make_response(jsonify({"error": "Username is required"}), 400)
+    new_user = request.json
+    username = new_user.get("username")
 
-    username = data["username"]
-    if username in users:
-        return make_response(jsonify({"error": "Username already exists"}), 400)
-
-    users[username] = data
-    return make_response(jsonify({"message": "User added", "user": data}), 201)
+    if not username:
+        return (jsonify({"error": "Username is required"}), 400)
+    if username == users:
+        return (jsonify({"error": "User already exists"}), 400)
+    users[username] = new_user
+    return jsonify({"message": "User added", "user": new_user}), 201
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=TRUE)
