6152d9297b4acc3d5d6905ad7a2faff169a91c62
Falbert19 <fernandoalbert19@gmail.com>
2025-02-23 20:22:56 -0400

    task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -52,7 +52,11 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(info).encode())
 
         else:
-            self.send_error(404, "Endpoint not found")
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
 
 
 if __name__ == "__main__":
