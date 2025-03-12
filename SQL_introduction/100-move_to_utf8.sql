ba90409103b525d43b5f331cc7e41d8b75aef116
Jesus A <blackyizus@YIZUS-MAC.local>
2025-03-11 21:19:45 -0400

    advance 1

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,44 +1,11 @@
-Correct conversion
-[copy_files] Filed copied: main_0_0.sql
-[copy_files] Filed copied: main_0_1.sql
-[exec_bash_compare] Command to run:
-service mysql start > /dev/null 2>&1
-timeout 30 bash -c 'service mysql start > /dev/null 2>&1'
-[exec_bash_compare] Return code: 0
-[exec_bash_compare] Student stdout:
-[exec_bash_compare] Student stdout length: 0
-[exec_bash_compare] Student stderr:
-[exec_bash_compare] Student stderr length: 0
-[exec_bash_compare] Desired stdout:
-[exec_bash_compare] Desired stdout length: 0
-[exec_bash_compare] Command to run:
-/usr/bin/mysqladmin -u root -p"root" password "root" > /dev/null 2>&1
-timeout 30 bash -c '/usr/bin/mysqladmin -u root -p"root" password "root" > /dev/null 2>&1'
-[exec_bash_compare] Return code: 0
-[exec_bash_compare] Student stdout:
-[exec_bash_compare] Student stdout length: 0
-[exec_bash_compare] Student stderr:
-[exec_bash_compare] Student stderr length: 0
-[exec_bash_compare] Desired stdout:
-[exec_bash_compare] Desired stdout length: 0
-[exec_bash_compare] Command to run:
-cat main_0_0.sql | mysql -hlocalhost -uroot -proot > /dev/null 2>&1
-timeout 30 bash -c 'cat main_0_0.sql | mysql -hlocalhost -uroot -proot > /dev/null 2>&1'
-[exec_bash_compare] Return code: 0
-[exec_bash_compare] Student stdout:
-[exec_bash_compare] Student stdout length: 0
-[exec_bash_compare] Student stderr:
-[exec_bash_compare] Student stderr length: 0
-[exec_bash_compare] Desired stdout:
-[exec_bash_compare] Desired stdout length: 0
-[exec_bash_compare] Command to run:
-cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -proot 2>&1 | grep -v '''Using a password'''
-timeout 30 bash -c 'cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -proot 2>&1 | grep -v '"'"''"'"''"'"'Using a password'"'"''"'"''"'"''
-[exec_bash_compare] Return code: 0
-[exec_bash_compare] Student stdout:
-ERROR 1046 (3D000) at line 5: No database selected
-[exec_bash_compare] Student stdout length: 51
-[exec_bash_compare] Student stderr:
-[exec_bash_compare] Student stderr length: 0
-[exec_bash_compare] Desired stdout:
-[exec_bash_compare] Desired stdout length: 0
+-- Select the database first
+USE hbtn_0c_0;
+
+-- Convert the database to UTF8MB4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table to UTF8MB4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the column "name" to UTF8MB4
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
