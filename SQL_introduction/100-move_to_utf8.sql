312ec59f2d119b0edfa1622d2f21ececbc0b73b8
Jesus A <blackyizus@YIZUS-MAC.local>
2025-03-11 21:28:25 -0400

    rip

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,8 @@
--- Select the database first
-USE hbtn_0c_0;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the database to UTF8MB4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Select the database
+USE hbtn_0c_0;
 
 -- Convert the table to UTF8MB4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
