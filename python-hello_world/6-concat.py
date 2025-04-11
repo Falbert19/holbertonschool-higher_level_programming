30c31033240c9294b315fee425805f67b0bf60a8
Falbert19 <fernandoalbert19@gmail.com>
2025-04-11 19:20:44 -0400

    cambio de objeto

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-print(f"Welcome to {str1} {str2}!")
+palabra1 = "Holberton"
+palabra2 = "School"
+print(f"Welcome to {palabra1} {palabra2}!")
